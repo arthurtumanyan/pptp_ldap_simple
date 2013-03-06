f388a64f891bcaf13b71a24c69aacd1bcd5c9c46
Arthur Tumanyan <arthurtumanyan@yahoo.com>
2013-03-06 17:29:26 +0400

    Update pptp_ldap_simple.h

--- pptp_ldap_simple.h
+++ pptp_ldap_simple.h
@@ -1,7 +1,19 @@
 /* 
- * File:   pptp_ldap_simple.h
- * Arthur Tumanyan <arthurtumanyan@yahoo.com>
- *
+ File:   pptp_ldap_simple.c
+ Copyright (C) 2013  Arthur Tumanyan <arthurtumanyan@yahoo.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef PPTP_LDAP_SIMPLE_H
